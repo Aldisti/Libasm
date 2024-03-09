b0c991d0fd52ecc91ceda903e1572c9967e79be2
aldisti <alessandrodiste99@gmail.com>
2024-03-09 13:54:13 +0100

    i can call asm from c

--- tutorials/hello.asm
+++ tutorials/hello.asm
@@ -9,9 +9,9 @@ section .data
 	text db "Hello, World!", 10
 
 section .text
-	global _start
+	global _foo
 
-_start:
+_foo:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, text
@@ -21,4 +21,7 @@ _start:
 	mov rax, 60
 	mov rdi, 42
 	syscall
+
+	mov rax, 42
+	ret
 
---
+++ tutorials/main.c
@@ -0,0 +1,8 @@
+
+extern int	_foo(void);
+
+int	main(void)
+{
+	return (_foo());
+}
+
