887f27b468a1db963998138ec07522a62201d8cc
aldisti <alessandrodiste99@gmail.com>
2024-03-23 12:50:35 +0100

    ft_list_foreach done

--- linked_list/main.c
+++ linked_list/main.c
@@ -21,6 +21,7 @@ extern t_list	*ft_list_push_strs(int size, char **);
 extern void		ft_list_clear(t_list *, void (*)(void *));
 extern t_list	*ft_list_at(t_list *, unsigned int);
 extern void		ft_list_reverse(t_list **);
+extern void		ft_list_foreach(t_list *, void (*)(void *));
 
 t_list	*create_list(int size, void *data)
 {
@@ -227,18 +228,43 @@ void	test_list_reverse(void)
 
 	printf("--- ft_list_reverse ---\n");
 	head = create_list(size, 0);
+	t_list	*node = head;
+	for (int i = 0; i < size; i++, node=node->next)
+		node->data = &arr[i];
+	ft_list_reverse(&head);
+	node = head;
+	for (int i = size - 1; node; node=node->next, i--)
+		TEST(node->data == &arr[i])
+	delete_list(head);
+}
+
+void	_add(void *data)
+{
+	int	*n;
+
+	n = (int *)data;
+	(*n)++;
+}
+
+void	test_list_foreach(void)
+{
+	int	arr[] = {1, 2, 3, 4, 5};
+	int	size = (int)(sizeof(arr) / sizeof(int));
+	t_list	*head = 0;
+
+	printf("--- ft_list_foreach ---\n");
+	head = create_list(size, 0);
 	{
 		t_list	*node = head;
 		for (int i = 0; i < size; i++, node=node->next)
 			node->data = &arr[i];
 	}
-	ft_list_reverse(&head);
+	ft_list_foreach(head, _add);
 	{
 		t_list	*node = head;
-		for (int i = size - 1; node; node=node->next, i--)
-			TEST(node->data == &arr[i])
+		for (int i = 0; node; node=node->next, i++)
+			TEST(*((int *)node->data) == i + 2)
 	}
-	delete_list(head);
 }
 
 int	main(void)
@@ -252,6 +278,7 @@ int	main(void)
 	test_list_clear();
 	test_list_at();
 	test_list_reverse();
+	test_list_foreach();
 	return (0);
 }
 
