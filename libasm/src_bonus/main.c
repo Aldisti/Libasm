dcdc16aeff6b9d4a55ecbccc1d67cad60d04fe95
aldisti <alessandrodiste99@gmail.com>
2024-04-03 22:10:37 +0200

    ft_atoi_base added to libasm

--- libasm/src_bonus/main.c
+++ libasm/src_bonus/main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define TEST(statement) (statement) ? write(1, "OK\n", 3) : write(1, "KO\n", 3);
+#define TEST_ATOI(num, base) printf("%-20s=> %d\n", num, ft_atoi_base(num, base));
 
 typedef struct s_list
 {
@@ -32,6 +33,7 @@ extern void		ft_list_sort(t_list **, int (*)());
 extern void		ft_list_reverse_fun(t_list *);
 extern void		ft_sorted_list_insert(t_list **, void *, int (*)());
 extern void		ft_sorted_list_merge(t_list **, t_list *, int (*)());
+extern int		ft_atoi_base(const char *str, const char *base);
 
 extern void		print_list(t_list *);
 
@@ -577,6 +579,62 @@ void	test_sorted_list_merge(void)
 	delete_list(head1);
 }
 
+void	test_atoi_base(void)
+{
+	const char	base10[] = "0123456789";
+	const char	base2[] = "01";
+	const char	base2a[] = "?h";
+	const char	base3[] = "012";
+	const char	base3a[] = "*&/";
+
+	printf("\t----- %s -----\n", base10);
+	TEST_ATOI("123", base10)
+	TEST_ATOI("+-+--+123", base10)
+	TEST_ATOI("---123abc", base10)
+	TEST_ATOI("", base10)
+	TEST_ATOI("abc", base10)
+	TEST_ATOI("--abc", base10)
+	TEST_ATOI("--abc123", base10)
+	TEST_ATOI("-- 123", base10)
+	TEST_ATOI("    123", base10)
+	TEST_ATOI(" - - - 123", base10)
+	TEST_ATOI("    -123", base10)
+	TEST_ATOI("    -abc123", base10)
+	TEST_ATOI("  abc  123", base10)
+	TEST_ATOI(" ---+--+1234ab567", base10)
+	TEST_ATOI("00000", base10)
+	TEST_ATOI("1", base10)
+	TEST_ATOI("10", base10)
+
+	printf("\t----- %s -----\n", base2);
+	TEST_ATOI("-01010110", base2)
+	TEST_ATOI("11111111", base2)
+	TEST_ATOI("00000000", base2)
+	TEST_ATOI("-10101010", base2)
+	TEST_ATOI("-01010101", base2)
+	TEST_ATOI("00000001", base2)
+	TEST_ATOI("00000011", base2)
+
+	printf("\t----- %s -----\n", base2a);
+	TEST_ATOI("-?h?h?hh?", base2a)
+	TEST_ATOI("hhhhhhhh", base2a)
+	TEST_ATOI("????????", base2a)
+	TEST_ATOI("-h?h?h?h?", base2a)
+	TEST_ATOI("-?h?h?h?h", base2a)
+	TEST_ATOI("???????h", base2a)
+	TEST_ATOI("??????hh", base2a)
+
+	// 012
+	printf("\t----- %s -----\n", base3);
+	TEST_ATOI("102", base3)
+	TEST_ATOI("221102", base3)
+
+	// *&/
+	printf("\t----- %s -----\n", base3a);
+	TEST_ATOI("&*/", base3a)
+	TEST_ATOI("//&&*/", base3a)
+}
+
 int	main(void)
 {
 	test_list_size();
@@ -599,6 +657,7 @@ int	main(void)
 	test_list_reverse_fun();
 	test_sorted_list_insert();
 	test_sorted_list_merge();
+	test_atoi_base();
 	return (0);
 }
 
